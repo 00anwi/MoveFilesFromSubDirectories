019359914335d5f3533ad684941ce4ea8ef48eeb
Andreas <andreas_s_w@hotmail.com>
2021-07-16 00:31:49 +0200

    Added another progressbar and file info text

--- WinFormsTestApp/Form1.Designer.cs
+++ WinFormsTestApp/Form1.Designer.cs
@@ -36,12 +36,17 @@ namespace WinFormsTestApp
             this.BTN_Run = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.TB_MoveDirectory = new System.Windows.Forms.TextBox();
-            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.progressBarTotal = new System.Windows.Forms.ProgressBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.progressBarCurrent = new System.Windows.Forms.ProgressBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.LBL_CurrentFileProgress = new System.Windows.Forms.Label();
+            this.LBL_TotalProgress = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // TB_SourceDirectory
             // 
-            this.TB_SourceDirectory.Location = new System.Drawing.Point(129, 25);
+            this.TB_SourceDirectory.Location = new System.Drawing.Point(129, 15);
             this.TB_SourceDirectory.Name = "TB_SourceDirectory";
             this.TB_SourceDirectory.Size = new System.Drawing.Size(383, 23);
             this.TB_SourceDirectory.TabIndex = 0;
@@ -49,7 +54,7 @@ namespace WinFormsTestApp
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 28);
+            this.label1.Location = new System.Drawing.Point(12, 18);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(97, 15);
             this.label1.TabIndex = 1;
@@ -57,7 +62,7 @@ namespace WinFormsTestApp
             // 
             // BTN_Run
             // 
-            this.BTN_Run.Location = new System.Drawing.Point(356, 95);
+            this.BTN_Run.Location = new System.Drawing.Point(360, 100);
             this.BTN_Run.Name = "BTN_Run";
             this.BTN_Run.Size = new System.Drawing.Size(156, 65);
             this.BTN_Run.TabIndex = 2;
@@ -68,7 +73,7 @@ namespace WinFormsTestApp
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 57);
+            this.label2.Location = new System.Drawing.Point(12, 47);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(91, 15);
             this.label2.TabIndex = 4;
@@ -76,24 +81,72 @@ namespace WinFormsTestApp
             // 
             // TB_MoveDirectory
             // 
-            this.TB_MoveDirectory.Location = new System.Drawing.Point(129, 54);
+            this.TB_MoveDirectory.Location = new System.Drawing.Point(129, 44);
             this.TB_MoveDirectory.Name = "TB_MoveDirectory";
             this.TB_MoveDirectory.Size = new System.Drawing.Size(383, 23);
             this.TB_MoveDirectory.TabIndex = 3;
             // 
-            // progressBar
+            // progressBarTotal
             // 
-            this.progressBar.Location = new System.Drawing.Point(12, 112);
-            this.progressBar.Name = "progressBar";
-            this.progressBar.Size = new System.Drawing.Size(338, 23);
-            this.progressBar.TabIndex = 5;
+            this.progressBarTotal.Location = new System.Drawing.Point(12, 142);
+            this.progressBarTotal.Name = "progressBarTotal";
+            this.progressBarTotal.Size = new System.Drawing.Size(338, 23);
+            this.progressBarTotal.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Total Progress: ";
+            // 
+            // progressBarCurrent
+            // 
+            this.progressBarCurrent.Location = new System.Drawing.Point(12, 99);
+            this.progressBarCurrent.Name = "progressBarCurrent";
+            this.progressBarCurrent.Size = new System.Drawing.Size(338, 23);
+            this.progressBarCurrent.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 81);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(119, 15);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Current File Progress:";
+            // 
+            // LBL_CurrentFileProgress
+            // 
+            this.LBL_CurrentFileProgress.AutoSize = true;
+            this.LBL_CurrentFileProgress.Location = new System.Drawing.Point(138, 81);
+            this.LBL_CurrentFileProgress.Name = "LBL_CurrentFileProgress";
+            this.LBL_CurrentFileProgress.Size = new System.Drawing.Size(38, 15);
+            this.LBL_CurrentFileProgress.TabIndex = 9;
+            this.LBL_CurrentFileProgress.Text = "label5";
+            // 
+            // LBL_TotalProgress
+            // 
+            this.LBL_TotalProgress.AutoSize = true;
+            this.LBL_TotalProgress.Location = new System.Drawing.Point(104, 125);
+            this.LBL_TotalProgress.Name = "LBL_TotalProgress";
+            this.LBL_TotalProgress.Size = new System.Drawing.Size(38, 15);
+            this.LBL_TotalProgress.TabIndex = 10;
+            this.LBL_TotalProgress.Text = "label6";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(528, 177);
-            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.LBL_TotalProgress);
+            this.Controls.Add(this.LBL_CurrentFileProgress);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.progressBarCurrent);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.progressBarTotal);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.TB_MoveDirectory);
             this.Controls.Add(this.BTN_Run);
@@ -103,6 +156,7 @@ namespace WinFormsTestApp
             this.Name = "Form1";
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.Text = "Flytta Filer App";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -117,7 +171,12 @@ namespace WinFormsTestApp
         private System.Windows.Forms.Button BTN_Run;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox TB_MoveDirectory;
-        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.ProgressBar progressBarTotal;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ProgressBar progressBarCurrent;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label LBL_CurrentFileProgress;
+        private System.Windows.Forms.Label LBL_TotalProgress;
     }
 }
 
--- WinFormsTestApp/Form1.cs
+++ WinFormsTestApp/Form1.cs
@@ -1,11 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.IO;
-using System.Linq;
-using System.Text;
+using System.Net;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -16,6 +13,19 @@ namespace WinFormsTestApp
         public Form1()
         {
             InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LBL_CurrentFileProgress.Text = "";
+            LBL_TotalProgress.Text = "";
+            FileCopyProgress += Form1_FileCopyProgress;
+        }
+
+        private void Form1_FileCopyProgress(int Int)
+        {
+            progressBarCurrent.Value = Int;
         }
 
         private void BTN_Run_Click(object sender, EventArgs e)
@@ -30,22 +40,62 @@ namespace WinFormsTestApp
             float progressPerFile = 100 / subDirectorys.Length;
             float currentProgress = 0;
 
+            var TotalProgress = 1;
             foreach (var directory in subDirectorys)
             {
                 var files = Directory.GetFiles(directory);
                 if (files.Length == 1)
                 {
                     var moveDestination = Path.Combine(moveDirectory, new DirectoryInfo(System.IO.Path.GetDirectoryName(files[0])).Name + Path.GetExtension(files[0]));
-                    File.Copy(files[0], moveDestination, true);
+                    LBL_CurrentFileProgress.Text = $"{Path.GetFileName(moveDestination)}";
+                    Application.DoEvents();
+                    FileDownloadComplete = false;
+                    CopyFileWithProgress(files[0], moveDestination);
+                    while (FileDownloadComplete == false)
+                    {
+                        Application.DoEvents();
+                    }
                 }
                 currentProgress += progressPerFile;
 
-                progressBar.Value = (int)currentProgress;
+                progressBarTotal.Value = (int)currentProgress;
+                LBL_TotalProgress.Text = $"{TotalProgress} of {subDirectorys.Length}";
+                TotalProgress++;
                 Application.DoEvents();
             }
 
-            progressBar.Value = 100;
+
+            progressBarTotal.Value = 100;
             Application.DoEvents();
+
+            MessageBox.Show("Move Files Complete!!!");
         }
+
+        static bool FileDownloadComplete = false;
+
+        public delegate void IntDelegate(int Int);
+
+        public static event IntDelegate FileCopyProgress;
+        public static void CopyFileWithProgress(string source, string destination)
+        {
+            var webClient = new WebClient();
+            webClient.DownloadProgressChanged += DownloadProgress;
+            webClient.DownloadFileCompleted += WebClient_DownloadFileCompleted;
+            webClient.DownloadFileAsync(new Uri(source), destination);
+        }
+
+        private static void WebClient_DownloadFileCompleted(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
+        {
+            FileDownloadComplete = true;
+        }
+
+        private static void DownloadProgress(object sender, DownloadProgressChangedEventArgs e)
+        {
+            if (FileCopyProgress != null)
+            {
+                FileCopyProgress(e.ProgressPercentage);
+            }
+        }
+
     }
 }
